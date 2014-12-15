5e4df27d2e4e899fcc3ec12a8435b90e7f5cc63f
Rhony Pedraza <rhony25@gmail.com>
2014-12-15 00:42:58 -0500

    Lista información de alojamiento

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-/*
-//Converter Class
-var Converter = require("csvtojson").core.Converter;
-var fs = require("fs");
-
-var csvFileName = "./files/establecimientosdealojamiento.csv";
-var fileStream = fs.createReadStream(csvFileName);
-//new converter instance
-var csvConverter = new Converter({
-	constructResult: true,
-	delimeter: ";"
-});
-
-//end_parsed will be emitted once parsing finished
-csvConverter.on("end_parsed",function(jsonObj){
-   console.log(jsonObj); //here is your result json object
-});
-
-//read from file
-fileStream.pipe(csvConverter);
-*/
-
-/*
-//var columns = ["column1", "column2", "column3"];
-var columns = ["ID", "RNT", "NOMBRE COMERCIAL", "TIPO", "DIRECCION", "LOCALIDAD", "TELEFONO", "E MAIL", "PAGINA WEB", "RANGO TARIFA", "ZONA", "ESTADO"];
-require("csv-to-array")({
-   file: "./files/establecimientosdealojamiento.csv",
-   columns: columns
-}, function (err, array) {
-  console.log(err || array);
-});
-*/
-
 var csv = require("ya-csv");
+var request = require("request");
+
 var reader = csv.createCsvFileReader("./files/establecimientosdealojamiento.csv", {
-    'separator': ';',
-    'columnsFromHeader': true
-    //'quote': '"',
-    //'escape': '"',       
-    //'comment': '',
+  separator: ";",
+  columnsFromHeader: true
 });
-//var writer = new csv.CsvWriter(process.stdout);
-reader.addListener('data', function(data) {
-    //writer.writeRecord([ data[0] ]);
-    console.log(data);
+
+var writer = csv.createCsvFileWriter("./files/establecimientosdealojamiento-procesado.csv", {
+  separator: ";",
+  quote: ""
+});
+
+reader.addListener("data", function(data) {
+  var direccion = data.DIRECCION;
+  if(direccion) {
+    console.log("direccion", direccion);
+    var options = {
+      url: "http://www.direccionesbogota.com/ajax/search/co/bogota?query=" + direccion,
+      headers: {
+        "X-Requested-With": "XMLHttpRequest"
+      }
+    };
+    request(options, function(error, response, body) {
+      var result = JSON.parse(body);
+      var coord = result ? result.coordinates : undefined;
+      if(coord) {
+        writer.writeRecord([ data["NOMBRE COMERCIAL"], data.DIRECCION, data.TELEFONO, data["E MAIL"], data["PAGINA WEB"], coord[0], coord[1] ]);
+      }
+    });
+  }
+});
+
+reader.addListener("end", function() {
+  // fin de lectura del csv
 });
